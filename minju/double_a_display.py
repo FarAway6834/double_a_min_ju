093478847b622f08851b6932bdef9970712b6b52
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-02-28 23:19:41 +0900

    umm

--- minju/double_a_display.py
+++ minju/double_a_display.py
@@ -4,27 +4,6 @@ from sys import stdin as readline as __stdread__
 from sys import stdin as readline as __stdwrite__
 from pyximport import install as __init__
 __init__()
-from core import main, is__main__ #core.pyx
+from minju.displaylib import mainer
 
-cdef const str __main__ = "__main__"
-cdef str (*streT)()
-
-cdef bool is__main__(str x):
-    bool y
-    y = __main__ == x #plz set it opt c eq
-    return y
-
-cdef str rstriper(str x) nogil:
-    str y
-    y = x.rstrip()
-    return y
-
-cdef void main(streT pystdin, streT pystdout) nogil:
-    str data
-    data = rstriper(pystdin())
-    pystdout(data)
-    pystdout('\n')
-    pystdout(data)
-    pystdout('\n')
-
-if is__main__(__name__) main(__stdread__, __stdwrite__)
+mainer(__name__, __stdread__, __stdwrite__)
