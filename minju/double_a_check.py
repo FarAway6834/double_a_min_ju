8d0a57ebea597b9fa3b723d9d941222f0d3e9a69
FarAwayJams <159678961+FarAway6834@users.noreply.github.com>
2025-02-28 23:35:53 +0900

    mov script will to init

--- minju/double_a_check.py
+++ minju/double_a_check.py
@@ -2,8 +2,6 @@
 
 from sys import stdin as readline as __stdread__
 from sys import stdin as readline as __stdwrite__
-from pyximport import install as __init__
-__init__()
 from minju.displaylib import mainer
 
 mainer(__name__, __stdread__, __stdwrite__)
